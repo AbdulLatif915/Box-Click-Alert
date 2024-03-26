3cb8d144463d0533fa68099a04e900ffd82e583c
AbdulLatif915 <abdullatifqasmi619@gmail.com>
2024-03-26 21:11:33 +0500

    Box Click Alert

---
+++ app.js
@@ -0,0 +1,35 @@
+document.getElementById("box1").addEventListener("click", function() {
+    showAlert("Box 1");
+  });
+  
+  document.getElementById("box2").addEventListener("click", function() {
+    showAlert("Box 2");
+  });
+  
+  document.getElementById("box3").addEventListener("click", function() {
+    showAlert("Box 3");
+  });
+  
+  document.getElementById("box4").addEventListener("click", function() {
+    showAlert("Box 4");
+  });
+  
+  document.getElementById("box5").addEventListener("click", function() {
+    showAlert("Box 5");
+  });
+  
+  document.getElementById("box6").addEventListener("click", function() {
+    showAlert("Box 6");
+  });
+  
+  document.getElementById("box7").addEventListener("click", function() {
+    showAlert("Box 7");
+  });
+  
+  document.getElementById("box8").addEventListener("click", function() {
+    showAlert("Box 8");
+  });
+  
+  function showAlert(title) {
+    alert("You clicked on " + title);
+  }
